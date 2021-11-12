923c80caaf157c45f75fe29a791557f904796f2a
joshyap <josh@Annies-MacBook-Pro-2.local>
2021-11-11 21:10:31 -0600

    testing image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import dumbledore from './dumbledore.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={dumbledore} className="App-logo" alt="logo" style={{ borderRadius: `50%` }} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
